0e2de2ab2cc71166cd9f2b1650fc02ea8abd71a4
Steven Reis <steven.oliveira@ciss.com.br>
2024-04-07 13:38:30 -0300

    ajustado atualização do placar via websocket

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const Api = axios.create({
-  baseURL:
-    'https://scoreboard-backend-develop.onrender.com/api',
-    //'http://3.20.235.215:5000/api',
+  baseURL: 'https://scoreboard-backend-develop.onrender.com/api',
+  // `http://localhost:3333/api`,
+  // 'http://3.20.235.215:5000/api',
 })
